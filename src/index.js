c1f0700bf31c90ab831bae4706d850b8090a30d2
Casey Collins <collinscasey.homes@gmail.com>
2022-10-18 00:19:02 -0600

    commit done

--- src/index.js
+++ src/index.js
@@ -3,13 +3,59 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import logo from './logo.svg';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   // <React.StrictMode>
+//   //   <App />
+//   // </React.StrictMode>
+//   <div>
+//     <h1> Does this work instead?</h1>
+//     <h5>oh shit it does</h5>
+
+//   </div>
+// );
+// const title = (
+//   <div>
+//     <h1> Does this work instead?</h1>
+//     <h5>oh shit it does</h5>
+
+//   </div>
+// )
+
+// ReactDOM.createRoot(document.getElementById('root')).render(title)
+
+const page = (
+  <div className="page">
+    <img src={logo} className="App-logo" alt="logo" />
+    <b>
+
+      <h1>  Fun facts about react </h1>
+    </b>
+
+    <ul>
+      <li> Was first released in 2013</li>
+      <li>Was originally created by Jordan Walke</li>
+      <li>Has well over 100K stars on Github</li>
+      <li>Is maintained by Meta</li>
+      <li> Power thousands of enterprise apps, including mobile apps</li>
+    </ul>
+  </div>
+)
+ReactDOM.createRoot(document.getElementById('root'))?.render((page))
+
+
+
+
+
+
+
+
+
+
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
